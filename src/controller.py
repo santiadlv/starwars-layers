87750d83f494f3a65d3f1adbdf4cec23520774fb
santiadlv <49079265+santiadlv@users.noreply.github.com>
2023-03-26 21:16:47 -0600

    Add async functions to get all character names

--- src/controller.py
+++ src/controller.py
@@ -1,3 +1,20 @@
-def sortById(movies: list) -> list:
+import httpx
+import asyncio
+
+
+def sort_by_id(movies: list) -> list:
     return movies.sort(key=lambda x: x['id'])
 
+async def get_character_name(client: httpx.AsyncClient, url: str) -> str:
+    response = await client.get(url)
+    character = response.json()
+    return character['name']
+
+async def get_all_movie_character_names(urls: list) -> list:
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        for url in urls:
+            tasks.append(asyncio.ensure_future(get_character_name(client, url)))
+            
+        character_names = await asyncio.gather(*tasks)
+        return character_names
